29d70e6cfcbcfd4988cca0a306c1e878248736c4
practicant1 <practicant1@MININT-D2NERBJ.net.work>
2016-06-30 11:52:01 +0300

    Al treilea

---
+++ Dummy/src/main/java/Pack1/Test1.java
@@ -0,0 +1,14 @@
+package Pack1;
+
+public class Test1 {
+	public static void main(String[] args)
+	{
+		System.out.println("Hello World!");
+		int a=10;
+		int b=5;
+		int c;
+		
+		c=a-b;
+		System.out.println(c);
+	}
+}
